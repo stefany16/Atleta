14cc6023790dc7ee28729d18e4c8e9401d027e2e
Stefany Silva <stefanysilvaoliver16@gmail.com>
2020-11-23 19:55:36 +0000

    atulizada

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 class Main {
-  public static void main(String[] args) {
-   //TesteSimples teste = new TesteSimples();
+  public static void main(String[] args) throws Exception {
+    
+
+    //TesteSimples teste = new TesteSimples();
     TestUnitario tu = new TestUnitario();  
     tu.executarAll();
+
+    TelaConsoleMenu tela = new TelaConsoleMenu();
+    tela.executar();
+
+
+
 
     
     System.out.println("\nimcmonitor, fim!");
